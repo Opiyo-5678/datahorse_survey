2a1931295387fd8d312e2eae3ea846bc437e7e5e
Opiyo-5678 <austineopiyo84@gmail.com>
2026-02-20 19:18:41 +0300

    auto create superuser in build

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser automatically if it doesn't exist
+python manage.py shell << 'EOF'
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin12').exists():
+    User.objects.create_superuser('admin12', 'vitalisopiyo84@gmail.com', 'Data@1234')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+EOF
